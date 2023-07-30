4ab32b94812b4d59493b71358cffcd6ad9ee995c
brandon-burke <b.burke023@gmail.com>
2023-07-30 16:35:35 -0400

    added more instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,12 @@
-use crate::memory::Memory;
+use crate::memory::{Memory, self};
 
-enum Flags {
+pub enum Conditions {
     Zero,
     Subtraction,
     HalfCarry,
     Carry,
+    NotZero,
+    NotCarry,
 }
 
 #[derive(Debug)]
@@ -125,7 +127,7 @@ impl Cpu {
     }
 
     pub fn get_zero_flag(&self) -> u8 {
-        return (self.f >> 7) & 1;
+        return (self.f >> 7) & 0x1;
     }
 
     pub fn set_zero_flag(&mut self) {
@@ -139,7 +141,7 @@ impl Cpu {
     }
 
     pub fn get_add_sub_flag(&self) -> u8 {
-        return (self.f >> 6) & 1;
+        return (self.f >> 6) & 0x1;
     }
 
     pub fn set_add_sub_flag(&mut self) {
@@ -153,7 +155,7 @@ impl Cpu {
     }
 
     pub fn get_half_carry_flag(&self) -> u8 {
-        return (self.f >> 5) & 1;
+        return (self.f >> 5) & 0x1;
     }
 
     pub fn set_half_carry_flag(&mut self) {
@@ -167,7 +169,7 @@ impl Cpu {
     }
 
     pub fn get_carry_flag(&self) -> u8 {
-        return (self.f >> 4) & 1;
+        return (self.f >> 4) & 0x1;
     }
 
     pub fn set_carry_flag(&mut self) {
@@ -387,6 +389,338 @@ impl Cpu {
     }
 
     /**
+     * Store SP & $FF at address n16 and SP >> 8 at address n16 + 1.
+     * 
+     * MACHINE CYCLES: 5
+     * INSTRUCTION LENGTH: 3
+     */
+    pub fn ld_u16_sp(&mut self, memory: &mut Memory) {
+        let lower_byte = memory.read_byte(self.pc);
+        self.pc += 1;
+        let upper_byte = memory.read_byte(self.pc);
+        self.pc += 1;
+
+        let address: u16 = ((upper_byte as u16) << 8) | lower_byte as u16;
+
+        memory.write_byte(address, Self::get_lower_byte(self.sp));
+        memory.write_byte(address + 1, Self::get_upper_byte(self.sp));
+    }
+
+    /**
+     * Add the value in r16 to HL.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1 
+     */
+    pub fn add_hl_r16(&mut self, high_reg: Register, low_reg: Register) {
+        let reg = match (&high_reg, &low_reg) {
+            (Register::B, Register::C) => self.bc(),
+            (Register::D, Register::E) => self.de(),
+            (Register::H, Register::L) => self.hl(),
+            _ => panic!("Cannot use {:?} and {:?} as a 16bit register", high_reg, low_reg),
+        };  
+        
+        let (result, overflow) = self.hl().overflowing_add(reg);
+        let half_carry_overflow = (self.hl() & 0x0FFF) + (reg & 0x0FFF) > 0x0FFF;
+
+        self.set_hl(result);
+        self.arithmetic_16bit_flag_update(result, half_carry_overflow, overflow);
+    }
+
+    /**
+     * Load value in register A from the byte pointed to by register r16.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn ld_a_r16(&mut self, memory: &Memory, high_reg: Register, low_reg: Register) {
+        let address = match (&high_reg, &low_reg) {
+            (Register::B, Register::C) => self.bc(),
+            (Register::D, Register::E) => self.de(),
+            (Register::H, Register::L) => self.hl(),
+            _ => panic!("Cannot use {:?} and {:?} as a 16bit register", high_reg, low_reg),
+        };  
+
+        self.a = memory.read_byte(address);
+    }
+
+    /**
+    * Decrement value in register r16 by 1.
+    * 
+    * MACHINE CYCLES: 2
+    * INSTRUCTION LENGTH: 1
+    */
+    pub fn dec_r16(&mut self, high_reg: Register, low_reg: Register) {
+        let value: u16;
+        match (&high_reg, &low_reg) {
+            (Register::B, Register::C) => { value = self.bc() - 1; self.set_bc(value); },
+            (Register::D, Register::E) => { value = self.de() - 1; self.set_de(value); },
+            (Register::H, Register::L) => { value = self.hl() - 1; self.set_hl(value); },
+            _ => panic!("Cannot use {:?} and {:?} as a 16bit register", high_reg, low_reg),
+        };  
+    }
+
+    /**
+    * Rotate register A right.
+    * 
+    * MACHINE CYCLE: 1
+    * INSTRUCTION LENGTH: 1
+    */
+    pub fn rrca(&mut self) {
+        let rotated_bit: u8 = Self::get_bit(self.a, 0);
+
+        self.a = (self.a >> 1) | (rotated_bit << 7);
+
+        self.reset_zero_flag();
+        self.reset_add_sub_flag();
+        self.reset_half_carry_flag();
+
+        if rotated_bit != 0 {
+            self.set_carry_flag();
+        } else {
+            self.reset_carry_flag();
+        }
+    }
+
+    /**
+     * THIS IS VERY SPECIAL NEED TO KNOW MORE ABOUT IT
+     */
+    pub fn stop(&mut self) {
+        todo!();
+    }
+
+    /**
+     * Rotate register A left through carry.
+     * 
+     * MACHINE CYCLES: 1
+     * INSTRUCTION LENGTH: 1 
+     */
+    pub fn rla(&mut self) {
+        let rotated_bit: u8 = Self::get_bit(self.a, 7);
+
+        self.a = (self.a << 1) | self.get_carry_flag();
+
+        self.reset_zero_flag();
+        self.reset_add_sub_flag();
+        self.reset_half_carry_flag();
+        if rotated_bit != 0 {
+            self.set_carry_flag();
+        } else {
+            self.reset_carry_flag();
+        }
+    }
+
+    /**
+     * Jump by i8 to a different address relative to the pc 
+     * 
+     * MACHINE CYCLES: 3
+     * INSTRUCTION LENGTH: 2
+     */
+    pub fn jr_i8(&mut self, memory: &mut Memory) {
+        let relative_address: i8 = memory.read_byte(self.pc) as i8;
+        self.pc += 1;
+        self.pc = self.pc.wrapping_add_signed(relative_address as i16);
+    }
+
+    /**
+     * Rotate register A right through carry.
+     * 
+     * MACHINE CYCLES: 1
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn rra(&mut self) {
+        let rotated_bit: u8 = Self::get_bit(self.a, 0);
+
+        self.a = (self.a >> 1) | (self.get_carry_flag() << 7);
+
+        self.reset_zero_flag();
+        self.reset_add_sub_flag();
+        self.reset_half_carry_flag();
+
+        if rotated_bit != 0 {
+            self.set_carry_flag();
+        } else {
+            self.reset_carry_flag();
+        }
+    }
+
+    /**
+     * Relative Jump by i8 if condition cc is met.
+     * 
+     * MACHINE CYCLES: 3 IF TAKEN/ 2 IF NOT TAKEN
+     * INSTRUCTION LENGTH: 2
+     */
+    pub fn jr_cc_i8(&mut self, memory: &Memory, mut condition: u8) {
+        condition &= 0x1;   //doing this so we only see the first bit
+
+        if condition != 0 {
+            let relative_address = memory.read_byte(self.pc);
+            self.pc += 1;
+            self.pc = self.pc.wrapping_add_signed(relative_address as i16);
+        } else {
+            self.pc += 1;
+        }
+    }
+
+    /**
+     * Store value in register A into the byte pointed by HL and increment HL afterwards.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn ld_hli_a(&mut self, memory: &mut Memory) {
+        memory.write_byte(self.hl(), self.a);
+        self.set_hl(self.hl() + 1);
+    }
+
+    /**
+     * Decimal Adjust Accumulator to get a correct BCD representation after an arithmetic instruction.
+     * 
+     * MACHINE CYCLES: 1
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn daa(&mut self) {
+        todo!();
+    }
+
+    /**
+     * Load value into register A from the byte pointed by HL and increment HL afterwards.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn ld_a_hli(&mut self, memory: &Memory) {
+        let hl_data = memory.read_byte(self.hl());
+        self.a = hl_data;
+        self.set_hl(self.hl() + 1);
+    }
+
+    /**
+     * Store the complement of the A register into the A register
+     * 
+     * MACHINE CYCLES: 1
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn cpl(&mut self) {
+        self.a = !self.a;
+
+        self.set_add_sub_flag();
+        self.set_half_carry_flag();
+    }
+
+    /**
+     * Load value n16 into register SP.
+     * 
+     * MACHINE CYCLES: 3
+     * INSTRUCTION LENGTH: 3
+     */
+    pub fn ld_sp_u16(&mut self, memory: &Memory) {
+        let lower_byte = memory.read_byte(self.pc);
+        self.pc += 1;
+        let upper_byte = memory.read_byte(self.pc);
+        self.pc += 1;
+        
+        self.sp = ((upper_byte as u16) << 8) | lower_byte as u16;
+    }
+
+    /**
+     * Store value in register A into the byte pointed by HL and decrement HL afterwards.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn ld_hld_a(&mut self, memory: &mut Memory) {
+        memory.write_byte(self.hl(), self.a);
+        self.set_hl(self.hl() - 1);
+    }
+
+    /**
+     * Increment value in register SP by 1.
+     * 
+     * MACHINE CYCLES: 2
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn inc_sp(&mut self) {
+        self.sp += 1;
+    }
+
+    /**
+     * Increment the byte pointed to by HL by 1.
+     * 
+     * MACHINE CYCLES: 3
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn inc_hl(&mut self, memory: &mut Memory) {
+        let mut hl_data = memory.read_byte(self.hl());
+        hl_data += 1;
+        memory.write_byte(self.hl(), hl_data);
+
+        self.reset_add_sub_flag();
+
+        if hl_data == 0 {
+            self.set_zero_flag();
+        } else {
+            self.reset_zero_flag();
+        }
+
+        if (hl_data & 0xF) + 1 > 0xF {
+            self.set_half_carry_flag();
+        } else {
+            self.reset_half_carry_flag();
+        }
+    }
+
+    /**
+    * Decrement the byte pointed to by HL by 1.
+    * 
+    * MACHINE CYCLES: 3
+    * INSTRUCTION LENGTH: 1
+    */
+    pub fn dec_hl(&mut self, memory: &mut Memory) {
+        let mut hl_data = memory.read_byte(self.hl());
+        hl_data -= 1;
+        memory.write_byte(self.hl(), hl_data);
+
+        if hl_data == 0 {
+            self.set_zero_flag();
+        } else {
+            self.reset_zero_flag();
+        }
+
+        if hl_data & 0xF == 0xF {
+            self.set_half_carry_flag();
+        } else {
+            self.reset_half_carry_flag();
+        }
+
+        self.set_add_sub_flag();
+    }
+
+    /**
+     * Store value n8 into the byte pointed to by register HL.
+     * 
+     * MACHINE CYCLES: 3
+     * INSTRUCTION LENGTH: 2
+     */
+    pub fn ld_hl_u8(&mut self, memory: &mut Memory) {
+        let immediate = memory.read_byte(self.pc);
+        self.pc += 1;
+        memory.write_byte(self.hl(), immediate);
+    }
+
+    /**
+     * Set the carry flag
+     * 
+     * MACHINE CYCLE: 1
+     * INSTRUCTION LENGTH: 1
+     */
+    pub fn scf(&mut self) {
+        self.set_carry_flag();
+        self.reset_add_sub_flag();
+        self.reset_half_carry_flag();
+    }
+
+    /**
     * Given an opcode it will execute the instruction of the opcode
     */
     pub fn exexute(&mut self, opcode: u8, memory: &mut Memory) {
@@ -399,10 +733,62 @@ impl Cpu {
             0x05 => self.dec_r8(Register::B),
             0x06 => self.ld_r8_u8(memory, Register::B),
             0x07 => self.rlca(),
-            0x08 =>
+            0x08 => self.ld_u16_sp(memory),
+            0x09 => self.add_hl_r16(Register::B, Register::C),
+            0x0A => self.ld_a_r16(memory, Register::B, Register::C),
+            0x0B => self.dec_r16(Register::B, Register::C),
+            0x0C => self.inc_r8(Register::C),
+            0x0D => self.dec_r8(Register::C),
+            0x0E => self.ld_r8_u8(memory, Register::C),
+            0x0F => self.rrca(),
+            0x10 => self.stop(),
+            0x11 => self.ld_r16_u16(memory, Register::D, Register::E),
+            0x12 => self.ld_r16_a(memory, Register::D, Register::E),
+            0x13 => self.inc_r16(Register::D, Register::E),
+            0x14 => self.inc_r8(Register::D),
+            0x15 => self.dec_r8(Register::D),
+            0x16 => self.ld_r8_u8(memory, Register::D),
+            0x17 => self.rla(),
+            0x18 => self.jr_i8(memory),
+            0x19 => self.add_hl_r16(Register::D, Register::E),
+            0x1A => self.ld_a_r16(memory, Register::D, Register::E),
+            0x1B => self.dec_r16(Register::D, Register::E),
+            0x1C => self.inc_r8(Register::E),
+            0x1D => self.dec_r8(Register::E),
+            0x1E => self.ld_r8_u8(memory, Register::E),
+            0x1F => self.rra(),
+            0x20 => self.jr_cc_i8(memory, !self.get_zero_flag()),
+            0x21 => self.ld_r16_u16(memory, Register::H, Register::L),
+            0x22 => self.ld_hli_a(memory),
+            0x23 => self.inc_r16(Register::H, Register::L),
+            0x24 => self.inc_r8(Register::H),
+            0x25 => self.dec_r8(Register::H),
+            0x26 => self.ld_r8_u8(memory, Register::H),
+            0x27 => self.daa(),
+            0x28 => self.jr_cc_i8(memory, self.get_zero_flag()),
+            0x29 => self.add_hl_r16(Register::H, Register::L),
+            0x2A => self.ld_a_hli(memory),
+            0x2B => self.dec_r16(Register::H, Register::L),
+            0x2C => self.inc_r8(Register::L),
+            0x2D => self.dec_r8(Register::L),
+            0x2E => self.ld_r8_u8(memory, Register::L),
+            0x2F => self.cpl(),
+            0x30 => self.jr_cc_i8(memory, !self.get_carry_flag()),
+            0x31 => self.ld_sp_u16(memory),
+            0x32 => self.ld_hld_a(memory),
+            0x33 => self.inc_sp(),
+            0x34 => self.inc_hl(memory),
+            0x35 => self.dec_hl(memory),
+            0x36 => self.ld_hl_u8(memory),
+            0x37 => self.scf(),
+            0x38 => self.jr_cc_i8(memory, self.get_carry_flag()),
+            0x39 => 
             _ => (),
         }
     }
+
+    //00000001 -> 11111110
+    //00000000 -> 11111111
 
 
 
@@ -523,20 +909,7 @@ impl Cpu {
         self.arithmetic_8bit_flag_update(result, half_carry_overflow, overflow);
     }
 
-    /**
-     * Adds the value in the passed 16-bit register to the 16-bit HL register
-     * and stores the result back in the HL register
-     * 
-     * MACHINE CYCLES: 2
-     * INSTRUCTION LENGTH: 1 BYTE
-     */
-    pub fn ADD_HL_r16(&mut self, reg: u16) {
-        let (result, overflow) = self.hl().overflowing_add(reg);
-        let half_carry_overflow = (self.hl() & 0x0FFF) + (reg & 0x0FFF) > 0x0FFF;
 
-        self.set_hl(result);
-        self.arithmetic_16bit_flag_update(result, half_carry_overflow, overflow);
-    }
 
     /**
      * Add the 16-bit value in SP to 16-bit HL register.
@@ -694,14 +1067,14 @@ impl Cpu {
      * MACHINE CYCLES: 6 if taken, 3 if not taken
      * INSTRUCTION LENGTH: 3
      */
-    pub fn CALL_cc_u16(&mut self, flag_type: Flags, address: u16, memory: &mut Memory) {
-        match flag_type {
-            Flags::Zero => if self.get_zero_flag() != 0 { self.CALL_n16(address, memory) },
-            Flags::Subtraction => if self.get_add_sub_flag() != 0 { self.CALL_n16(address, memory) },
-            Flags::HalfCarry => if self.get_half_carry_flag() != 0 { self.CALL_n16(address, memory) },
-            Flags::Carry => if self.get_carry_flag() != 0 { self.CALL_n16(address, memory) },
-        }
-    }
+    // pub fn CALL_cc_u16(&mut self, flag_type: Flags, address: u16, memory: &mut Memory) {
+    //     match flag_type {
+    //         Flags::Zero => if self.get_zero_flag() != 0 { self.CALL_n16(address, memory) },
+    //         Flags::Subtraction => if self.get_add_sub_flag() != 0 { self.CALL_n16(address, memory) },
+    //         Flags::HalfCarry => if self.get_half_carry_flag() != 0 { self.CALL_n16(address, memory) },
+    //         Flags::Carry => if self.get_carry_flag() != 0 { self.CALL_n16(address, memory) },
+    //     }
+    // }
 
     /**
      * Jump to address n16; effectively, store n16 into PC.
